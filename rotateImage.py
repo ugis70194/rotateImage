2b3b6160ff7049cf7273cd1fe26ec10aa7ceaf6d
ugis <ugis70194@yahoo.co.jp>
2019-06-19 16:13:08 +0900

    背景の色を合わせるようにした

--- rotateImage.py
+++ rotateImage.py
@@ -1,26 +1,31 @@
 from PIL import Image
 import sys, os, os.path
 
-def addMargin(img):
-    width, height = img.size
+def addMargin(img, width, height):
     left = width // 4
     top = height // 4
     newWidth = width + left*2
     newHeight = height + top*2
 
-    addedMargin = Image.new(img.mode, (newWidth, newHeight), (0, 0, 0))
+    color = img.getpixel((0, 0))
+
+    addedMargin = Image.new(img.mode, (newWidth, newHeight), color)
     addedMargin.paste(img, (left, top))
 
     return addedMargin
 
-def rotateImage(img, deg, num, saveDirPath, fileName, ext, qty):
+def rotateImage(img, deg, num, saveDirPath, fileName, ext, qty, width, height):
     if num > qty: return 
 
-    rotated = img.rotate(deg)
-    rotated.save(saveDirPath + '/' + fileName + str(num) + ext)
+    left = width // 4
+    top = height // 4
 
-    rotateImage(rotated, deg / 2, 2*num, saveDirPath, fileName, ext, qty)
-    rotateImage(rotated, -deg / 2, 2*num + 1, saveDirPath, fileName, ext, qty)
+    rotated = img.rotate(deg)
+    CropImg = rotated.crop((left,top,width+left,height+top));
+    CropImg.save(saveDirPath + '/' + fileName + str(num) + ext)
+
+    rotateImage(rotated, deg / 2, 2*num, saveDirPath, fileName, ext, qty, width, height)
+    rotateImage(rotated, -deg / 2, 2*num + 1, saveDirPath, fileName, ext, qty, width, height)
 
 def main():
     args = sys.argv
@@ -48,9 +53,15 @@ def main():
                 fileName = fileName[-len(fileName):-4:1]
 
                 img = Image.open(dirPath + '/' + fileName + ext)
-                img = addMargin(img)
+                width, height = img.size
+                left = width // 4
+                top = height // 4
 
-                img.save(saveDirPath + '/' + fileName + ext)
-                rotateImage(img, 180, 1, saveDirPath, fileName, ext, qty)
+                img = addMargin(img, width, height)
+
+                CropImg = img.crop((left,top,width+left,height+top));
+                CropImg.save(saveDirPath + '/' + fileName + ext)
+
+                rotateImage(img, 180, 1, saveDirPath, fileName, ext, qty, width, height)
 
 main()
